80c2b516588bd1e58bf2bb6a678bef997e518215
skala <skala>
2025-05-29 16:25:29 +0900

    Initial Spring Boot Project setup

---
+++ src/main/java/com/sk/growthnav/GrowthNavApplication.java
@@ -0,0 +1,13 @@
+package com.sk.growthnav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthNavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthNavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/growthnav/GrowthNavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.growthnav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthNavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
