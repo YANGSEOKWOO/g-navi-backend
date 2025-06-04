f2facc4661b3d0d1555401515c3fd1f7830c445f
YANGSEOKWOO <didtjrdn030@naver.com>
2025-06-04 11:35:07 +0900

    feat: Base Entity 작성

--- src/main/java/com/sk/growthnav/GrowthNavApplication.java
+++ src/main/java/com/sk/growthnav/GrowthNavApplication.java
@@ -2,12 +2,14 @@ package com.sk.growthnav;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class GrowthNavApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrowthNavApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GrowthNavApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/sk/growthnav/global/base/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.sk.growthnav.global.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity extends BaseTimeEntity {
+    @Column
+    private LocalDateTime deletedAt;
+
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+}
