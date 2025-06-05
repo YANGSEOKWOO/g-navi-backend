9f5dbdb87f8eee2db244b97877a36461532b6c6f
YANGSEOKWOO <didtjrdn030@naver.com>
2025-06-05 15:07:35 +0900

    Remove .gradle tracked files from Git

---
+++ src/main/java/com/sk/growthnav/global/init/DummyMemberInitializer.java
@@ -0,0 +1,11 @@
+//package com.sk.growthnav.global.init;
+//
+//import jakarta.annotation.PostConstruct;
+//
+//public class DummyMemberInitializer {
+//
+//    @PostConstruct
+//    public void init(){
+//        if(mem)
+//    }
+//}
