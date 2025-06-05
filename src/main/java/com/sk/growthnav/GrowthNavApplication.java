60e2f1ec8063aa8130145def12b081f9d4b4970b
YANGSEOKWOO <didtjrdn030@naver.com>
2025-06-05 12:38:54 +0900

    test: Test Profile 분리

--- src/main/java/com/sk/growthnav/GrowthNavApplication.java
+++ src/main/java/com/sk/growthnav/GrowthNavApplication.java
@@ -3,9 +3,13 @@ package com.sk.growthnav;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackages = "com.sk.growthnav.api.conversation.repository")
 public class GrowthNavApplication {
 
     public static void main(String[] args) {
