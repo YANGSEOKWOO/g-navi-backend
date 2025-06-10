03f9704fe8ec93291165f094601b4912bd34fba7
YANGSEOKWOO <didtjrdn030@naver.com>
2025-06-10 11:16:25 +0900

    fix: Cors구성

---
+++ src/main/java/com/sk/growthnav/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.sk.growthnav.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "https://sk-gnavi4.skala25a.project.skala-ai.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
